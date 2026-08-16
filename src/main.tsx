2cbf6f841d815a2269bae4b63b6a54cd5f703a33
tibouniseifeddine-creator <tibouniseifeddine@gmail.com>
2026-08-16 03:05:26 +0100

    chore: disable demo provider in production release

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { AuthProvider } from './contexts/AuthContext';
-import { DemoProvider } from './contexts/DemoContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LanguageProvider>
       <AuthProvider>
-        <DemoProvider>
-          <App />
-        </DemoProvider>
+        <App />
       </AuthProvider>
     </LanguageProvider>
   </React.StrictMode>
